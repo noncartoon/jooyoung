eea6e92aa85fc6bc6af711b9955f04618e988a15
noncartoon <58637706+noncartoon@users.noreply.github.com>
2019-12-08 02:06:18 +0700

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -2,92 +2,92 @@
 const Discord = require('discord.js');
 
 //Create client instance as bot
-const บอทพรี่คาซึยะ = new Discord.Client();
+const botบอทพรี่คาซึยะ = new Discord.Client();
 
 //Set listener on 'ready'
-บอทพรี่คาซึยะ.on('ready', () => {
+botบอทพรี่คาซึยะ.on('ready', () => {
   console.log('Rem ready!');
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'อีกระเทย') {
     message.reply('มึงสิกระเทย');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'สวัสดีครับ') {
     message.reply('ท่านสมาชิกคนชอบสี');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'น้ำทิพย์เป็นคนยังไง') {
     message.reply('เป็นกระเทย');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'แหนมเป็นคนยังไง') {
     message.reply('เป็นกระเทยไง');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'คมสันเป็นคนยังไง') {
     message.reply('เป็นอีกระเทย');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'โอ๊ตเป็นคนยังไง') {
     message.reply('อีกระเทย');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'มึงสิกระเทย') {
     message.reply('เดี๋ยวกูจะแจ้ง เดี๋ยวกูจะแจ้ง');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'พรี่แง้เป็นคนยังไง') {
     message.reply('ดี');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'นนท์เป็นคนยังไง') {
     message.reply('หล่อสุด');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'นนท์เป็นคนยังไง') {
     message.reply('สุดหล่อว่ะ');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'แจ๊ปเป็นคนยังไง') {
     message.reply('เป็นร้อนใน');
   }
 });
 
 //Set listener on 'message'
-บอทพรี่คาซึยะ.on('message', message => {
+botบอทพรี่คาซึยะ.on('message', message => {
   if (message.content === 'เปิดเพลง') {
     message.reply('ก็ใส่โค๊ดเพลงที่จะเปิดมาสิวะ กระเทย');
   }
